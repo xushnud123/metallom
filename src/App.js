8bba4fe39656ad2f77d4a10fc99dc9c166e774b1
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-08-09 15:57:53 +0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
+import firebase from "./components/firebase/firebase";
 import Cards from "./components/cards/cards";
 import Header from "./components/Header/header";
 import Products from "./components/Products/products";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const q = query(collection(firebase, "product"));
+    const unSub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) =>
+        todosArray.push({ ...doc.data(), id: doc.id })
+      );
+      setTodos(todosArray);
+    });
+    return () => unSub();
+  }, []);
+  console.log(todos)
   return (
     <React.Fragment>
       <Products />
-      <Cards/>
+      <Cards todos={todos}/>
     </React.Fragment>
   );
 }
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -3,22 +3,22 @@ import img from "../../assets/images/header-bg.jpg";
 
 const arr = [1, 2, 3, 4, 5, 6];
 
-const Cards = () => {
+const Cards = ({todos}) => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.cards}>
-        {arr.map((item) => (
+        {todos.map((item) => (
           <div key={item} className={classes.card}>
             <div className={classes.header}>
               <img src={img} alt="img not found" />
             </div>
             <div className={classes.body}>
               <div className={classes.wrap}>
-                <h1 className={classes.title}>Latum</h1>
-                <p className={classes.price}>цена: 2222₽</p>
+                <h1 className={classes.title}>{item.title}</h1>
+                <p className={classes.price}>цена: {item.number}₽</p>
               </div>
               <p className={classes.date}>
-                <span>Время:&nbsp;</span>09.09.2022
+                <span>Время:&nbsp;</span>{item.date}.{item?.month}.{item.year}
               </p>
             </div>
           </div>
