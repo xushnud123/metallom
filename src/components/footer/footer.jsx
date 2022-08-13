c377e1cfe820e0c6cefcd8f4e7502fda571c07fc
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-08-14 01:41:21 +0500

    footer finished

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,14 @@
 import classes from "./foooter.module.scss";
+import logo from '../../assets/images/Union.svg'
 
 const Footer = () => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.container}>
         <div className={classes.row}>
-            
+          <img src={logo} alt="img not found" />
+          <a href="#">Tel: +(998) 99 882 06 41</a>
+          <p>&copy; 2022 Xushnud, Все права защищены</p>
         </div>
       </div>
     </div>
